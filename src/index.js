524762b5f9a39bca6441a8d2dd46728d6d745efc
Tsimur Mirzayeu <timoha993@gmail.com>
2019-03-23 17:04:32 +0300

    feat: create a solution of the task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let answer = {};
+    const H = 50;
+    const Q = 25;
+    const D = 10;
+    const N = 5;
+    const P = 1;
+    if (currency <= 0) return answer;
+    if (currency >= 10000) return {
+        error: "You are rich, my friend! We don't have so much coins for exchange"
+    };
+    if (currency >= H) {
+        answer["H"] = Math.floor(currency / H);
+        currency = currency - Math.floor(answer["H"] * H);
+    }
+    if (currency >= Q) {
+        answer["Q"] = Math.floor(currency / Q);
+        currency = currency - Math.floor(answer["Q"] * Q);
+    }
+    if (currency >= D) {
+        answer["D"] = Math.floor(currency / D);
+        currency = currency - Math.floor(answer["D"] * D);
+    }
+    if (currency >= N) {
+        answer["N"] = Math.floor(currency / N);
+        currency = currency - Math.floor(answer["N"] * N);
+    }
+    if (currency >= P) {
+        answer["P"] = Math.floor(currency * P);
+    }
+    return (answer);
 }
